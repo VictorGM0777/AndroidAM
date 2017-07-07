6679f62bc45663923960c2a10db4667173e1906c
Victor <victor.guilherme100@gmail.com>
2017-07-07 15:40:39 -0300

    Continuacao tela de login.

--- AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/MainActivity.java
+++ AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.fiap.whiletrue.appam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void abreTela2(View v) {
+
+        Intent paraTela2 = new Intent(this, Main2Activity.class);
+        startActivity(paraTela2);
+
+    }
+
 }
