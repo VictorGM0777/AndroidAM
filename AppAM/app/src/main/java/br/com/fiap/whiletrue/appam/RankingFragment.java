164fc4b735edccde98f776b5b5d6ba89ebcfaea6
Victor <victor.guilherme100@gmail.com>
2017-07-20 18:27:43 -0300

    Desenvolvimento.

--- AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/RankingFragment.java
+++ AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/RankingFragment.java
@@ -2,11 +2,17 @@ package br.com.fiap.whiletrue.appam;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class RankingFragment extends Fragment implements View.OnClickListener {
+public class RankingFragment extends Fragment implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+
+    private RadioGroup rgRanking;
+    private TextView txtNomeConsultorRanking;
 
     public RankingFragment() {
 
@@ -18,11 +24,42 @@ public class RankingFragment extends Fragment implements View.OnClickListener {
         // v = tela do fragment
         View v = inflater.inflate(R.layout.fragment_ranking, container, false);
 
+        rgRanking = (RadioGroup) v.findViewById(R.id.radioRanking);
+        rgRanking.setOnCheckedChangeListener(this);
+
+        txtNomeConsultorRanking = (TextView) v.findViewById(R.id.txtNomeConsultorRanking);
+
         return v;
     }
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+
+        mudarTexto();
+
+    }
+
+    private void mudarTexto() {
+
+        int tam = this.rgRanking.getCheckedRadioButtonId();
+
+        switch (tam) {
+
+            case R.id.radioMensalRanking:
+                this.txtNomeConsultorRanking.setText(R.string.teste_mensal);
+                break;
+            case R.id.radioAnualRanking:
+                this.txtNomeConsultorRanking.setText(R.string.teste_anual);
+                break;
+            default:
+                break;
+
+        }
 
     }
 
