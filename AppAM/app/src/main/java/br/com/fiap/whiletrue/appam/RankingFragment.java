0f9b94949bfdc8e4a24e7d7d51d598b1afa4b0b0
Victor <victor.guilherme100@gmail.com>
2017-07-21 18:31:20 -0300

    Desenvolvimento.

--- AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/RankingFragment.java
+++ AppAM/app/src/main/java/br/com/fiap/whiletrue/appam/RankingFragment.java
@@ -13,6 +13,12 @@ public class RankingFragment extends Fragment implements View.OnClickListener, R
 
     private RadioGroup rgRanking;
     private TextView txtNomeConsultorRanking;
+    private TextView txtNomeConsultorRanking2;
+    private TextView txtNomeConsultorRanking3;
+    private TextView txtNomeConsultorRanking4;
+    private TextView txtNomeConsultorRanking5;
+    private TextView txtNomeConsultorRanking6;
+    private TextView txtNomeConsultorRanking7;
 
     public RankingFragment() {
 
@@ -28,6 +34,12 @@ public class RankingFragment extends Fragment implements View.OnClickListener, R
         rgRanking.setOnCheckedChangeListener(this);
 
         txtNomeConsultorRanking = (TextView) v.findViewById(R.id.txtNomeConsultorRanking);
+        txtNomeConsultorRanking2 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking2);
+        txtNomeConsultorRanking3 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking3);
+        txtNomeConsultorRanking4 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking4);
+        txtNomeConsultorRanking5 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking5);
+        txtNomeConsultorRanking6 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking6);
+        txtNomeConsultorRanking7 = (TextView) v.findViewById(R.id.txtNomeConsultorRanking7);
 
         return v;
     }
@@ -52,9 +64,21 @@ public class RankingFragment extends Fragment implements View.OnClickListener, R
 
             case R.id.radioMensalRanking:
                 this.txtNomeConsultorRanking.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking2.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking3.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking4.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking5.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking6.setText(R.string.teste_mensal);
+                this.txtNomeConsultorRanking7.setText(R.string.teste_mensal);
                 break;
             case R.id.radioAnualRanking:
                 this.txtNomeConsultorRanking.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking2.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking3.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking4.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking5.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking6.setText(R.string.teste_anual);
+                this.txtNomeConsultorRanking7.setText(R.string.teste_anual);
                 break;
             default:
                 break;
